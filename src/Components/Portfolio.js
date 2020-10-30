d0d84d54ac291af6cd1b53602643f3705c37a7a2
Antoni Sanchez Palacios <tonysapa90@outlook.com>
2020-10-30 19:49:13 +0100

    minor portfolio styles change

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/' + projects.image;
         
         return (
-          <div key={projects.title} className="columns portfolio-item" style={{width:'500px'}}>
+          <div key={projects.title} className="columns portfolio-item" style={{width:'50%'}}>
             <div className="item-wrap">
               <a href={projects.url} title={projects.title} target='_blank'>
                 <img alt={projects.title} src={projectImage} />
