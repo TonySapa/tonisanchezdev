7b129f95c83595ab222b62c0d919dd4e3ed27e68
Antoni Sanchez Palacios <antoniosapa90@gmail.com>
2021-01-15 14:26:29 +0100

    202101151426a Updated dokey docs info and site header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,27 +3,48 @@ import background1 from './images/background3c.png'
 import circledImage from './images/profilePicture1.png'
 //import ParticlesBg  from "particles-bg";
 
-const blue = {color:'rgb(135, 220, 226)'}
+const BackVector = () => {
+
+   const style = {
+
+   }
+
+   return (
+      <div style={style}>
+         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
+            <g fill-rule="evenodd">
+               <g fill="#000">
+                  <path opacity=".5" d="M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z" />
+                  <path d="M6 5V0H5v5H0v1h5v94h1V6h94V5H6z" />
+               </g>
+            </g>
+         </svg>
+      </div>
+   )
+}
 
 class Header extends Component {
 
   render() {
 
-    if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
+   if(this.props.data){
+      var project = this.props.data.project;
+      var github = this.props.data.github;
       var name = this.props.data.name;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      var welcomeTitle = this.props.data.welcomeTitle;
+      var description = this.props.data.description;
+      var city = this.props.data.address.city;
+      var networks = this.props.data.social.map(function(network){
+      return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
-    }
+   }
 
     return (
       <header id="home">
       {/*<ParticlesBg type="circle" bg={true} />*/}
       <img src={background1} alt='background image' className='back-style' />
+      {/* Optional green squares as background */}
+      {/*<BackVector />*/}
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -42,7 +63,7 @@ class Header extends Component {
          <img src={circledImage} alt='profile picture' className='circled-image' />
       
          <div className="banner-text">
-            <h1>{'console.log(`hello ${you}`)'}</h1>
+            <h1>{welcomeTitle}</h1>
             <h3>{description}</h3>
             <hr />
             <ul className="social">
